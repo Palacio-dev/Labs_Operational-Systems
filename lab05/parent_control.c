9012cb1d705a28ac3d0be4b5a451a032f547753d
lucasmnd <lucasrodriguesdemendonca@gmail.com>
2026-06-28 17:25:46 -0300

    Trazendo user strings pro kernel space de forma segura antes de processá-las

--- lab05/parent_control.c
+++ lab05/parent_control.c
@@ -1,14 +1,31 @@
+#include <linux/printk.h>
+#include <linux/slab.h>
+#include <linux/string.h>
 #include <linux/syscalls.h>
 #include "pc_linked_list.h"
 
 SYSCALL_DEFINE2(parent_control_change, unsigned long, remove, unsigned char __user *, url){
+
+    int ret;
+    unsigned char *safe_url = strndup_user(url, PC_STR_BUF_LEN);
+    
     if(remove){
-        return pc_list_remove(url);
+        ret = pc_list_remove(safe_url);
+    } else {
+        ret = pc_list_add(safe_url);
     }
 
-    return pc_list_add(url);
+    kfree(safe_url);
+
+    return ret;
 }
 
 SYSCALL_DEFINE1(parent_control_check, unsigned char __user *, url){
-    return pc_list_contains(url);
+    unsigned char *safe_url = strndup_user(url, PC_STR_BUF_LEN);
+
+    bool ret = pc_list_contains(safe_url);
+
+    kfree(safe_url);
+
+    return ret;
 }
